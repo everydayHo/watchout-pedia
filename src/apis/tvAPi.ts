1b96c98ea6fc936545b1fb557708e5a8fbb08c7f
everydayHo <jungwoopage@gmail.com>
2022-10-30 16:48:45 +0900

    themovedb api 생성

---
+++ src/apis/tvAPi.ts
@@ -0,0 +1,19 @@
+import axiosInstance from './index';
+
+export const latestApi = () => axiosInstance.get('/tv/latest');
+
+export const airingTodayApi = () => axiosInstance.get('/tv/airing_today');
+
+export const onTheAirApi = () => axiosInstance.get('/tv/on_the_air');
+
+export const topRatedApi = () => axiosInstance.get('/tv/top_rated');
+
+export const popularApi = () => axiosInstance.get('/tv/popular');
+
+export const detailApi = (tvId: string) => axiosInstance.get(`/tv/${tvId}`);
+
+export const similarApi = (tvId: string) =>
+	axiosInstance.get(`/movie/${tvId}/similar`);
+
+export const searchApi = (query: string) =>
+	axiosInstance.get(`/search/tv?query=${query}`);
